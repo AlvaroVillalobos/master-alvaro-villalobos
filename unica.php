3af8f775c83b263d361b8f3ace649d9356949e1d
Álvaro Villalobos <77793482+AlvaroVillalobos@users.noreply.github.com>
2026-01-26 20:48:00 +0100

    09 – 03 META ROBOTS
    Crea un meta robots que solo afecte a Google que:
    
    Sea indexable.
    Se puedan seguir los enlaces de la página.
    No permita más de 25 caracteres en la meta description.
    No se puedan indexar las imágenes.
    El contenido deje de estar indexable el 27/09/2033
    
    main -> unica.php

--- unica.php
+++ unica.php
@@ -3,6 +3,7 @@
     <head>
      <meta charset='UTF-8'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27">
      <title>Ejemplo de title</title>
      <meta name="description" content="blablabla">
     </head>
