83448e8d7573764c4ad54d1ff299f2dd0c23cb16
Álvaro Villalobos <77793482+AlvaroVillalobos@users.noreply.github.com>
2026-01-26 20:18:46 +0100

    09 – 02 METAETIQUETAS BÁSICAS

---
+++ unica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+     <meta charset='UTF-8'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <title>Ejemplo de title</title>
+     <meta name="description" content="blablabla">
+    </head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+        <h1>Página única</h1>
+    <footer>
+
+    </footer>
+
+</body>    
