77893c642fd9ca03aed9e7d56f09bab4681fc713
Álvaro Villalobos <77793482+AlvaroVillalobos@users.noreply.github.com>
2026-01-26 21:11:20 +0100

    09 – 05 HTTP EQUIV

--- unica.php
+++ unica.php
@@ -6,6 +6,34 @@
      <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27">
      <title>Ejemplo de title</title>
      <meta name="description" content="blablabla">
+     <!--<meta http-equiv="refresh" content="0; url=https://www.google.com">
+
+<meta name="robots" content="all">
+
+<meta name="robots" content="noindex">
+
+<meta name="robots" content="nofollow">
+
+<meta name="robots" content="none">
+
+<meta name="robots" content="nosnippet">
+
+<meta name="robots" content="noindex, indexifembedded">
+
+<meta name="robots" content="max-snippet:50">
+
+<meta name="robots" content="max-image-preview:large">
+
+<meta name="robots" content="max-video-preview:10">
+
+<meta name="robots" content="notranslate">
+
+<meta name="robots" content="noimageindex">
+
+<meta name="robots" content="unavailable_after: 2026-06-20">
+
+meta name="rating" content="adult">-->
+
     </head>
 
 <body>
